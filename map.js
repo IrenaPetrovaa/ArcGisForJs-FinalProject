6705ab509b6594ee5db1d170eab7dcc3c8db3ac2
Irena Petrova <irenapetrovag@gmail.com>
2023-05-18 18:05:49 +0300

    create web map; add home button

---
+++ map.js
@@ -0,0 +1,33 @@
+require([
+    "esri/config", 
+    "esri/WebMap", 
+    "esri/views/MapView",
+    "esri/widgets/ScaleBar",
+    "esri/widgets/Legend",
+    "esri/widgets/Home",
+    "esri/widgets/LayerList",
+    "esri/widgets/BasemapToggle",
+    "esri/widgets/BasemapGallery",
+    "esri/widgets/Search"
+], function(esriConfig, WebMap, MapView, ScaleBar,Legend, Home, LayerList, BasemapToggle, BasemapGallery, Search) {
+    esriConfig.apiKey = "AAPK11470563ebea40ca8dc828aabaa1ff34NHMnY0teh1E2ut_mvV-mFNRsb2nkwyUOwl5KxIy3YBm84ppxazR_vHgx4u5tFspW"
+
+    const webMap = new WebMap({
+        portalItem: {
+            id: "232b4d297d054b2a831a3ce629ac8495"
+        }
+    });
+
+    const view = new MapView({
+        container: "viewDiv",
+        map: webMap
+
+
+    });
+
+    const homeBtn = new Home({
+        view: view
+    });
+
+    view.ui.add(homeBtn, "top-left");
+})
