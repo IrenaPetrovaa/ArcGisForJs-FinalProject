2e6c8e27740ba5218c5817d79d2ee47ca59578f5
Irena Petrova <irenapetrovag@gmail.com>
2023-05-23 02:40:21 +0300

    add custom buttons; add layerlist

--- map.js
+++ map.js
@@ -45,6 +45,66 @@ require([
 
     view.ui.add(scaleBar, "bottom-right");
 
+    view.ui.add("basemap-btn", "top-right");
+    view.ui.add("layerList-btn", "top-right");
 
+    const basemapToggle = new BasemapToggle({
+        view: view,
+        nextBasemap: "arcgis-imagery"
+    })
+
+    const basemapGallery = new BasemapGallery({
+        view: view,
+        source: {
+            query: {
+
+            }
+        },
+    })
+
+    view.ui.add(basemapToggle, "bottom-right");
+    
+    const layerList = new LayerList({
+        view:view
+    })
+
+    view.ui.add(basemapGallery, "top-right");
+
+    view.ui.add(layerList, "top-right");
+
+    document
+    .getElementById("layerList-btn")
+    .addEventListener("click", function() {
+        toggleButton("layerList");
+    })
+
+    document
+    .getElementById("basemap-btn")
+    .addEventListener("click", function() {
+        toggleButton("gallery");
+    })
+
+    const searchWidget = new Search({
+        view:view
+    })
+
+    view.ui.add(searchWidget, "top-left");
+
+    function toggleButton (item) {
+        const layerListEl = document.getElementsByClassName("esri-layer-list")[0];
+        const galleryEl = document.getElementsByClassName("esri-basemap-gallery") [0];
+        let currentProp;
+
+
+        if(item =="layerList") {
+            currentProp = layerListEl.style.getPropertyValue("display");
+            layerListEl.style.setProperty("display", currentProp =="block" ? "none" : "block");
+            galleryEl.style.setProperty("display", "none");
+        } else if(item== "gallery") {
+            currentProp = galleryEl.style.getPropertyValue("display");
+            galleryEl.style.setProperty("display", currentProp =="block" ? "none" : "block");
+            layerListEl.style.setProperty("display", "none")
+        }
+    }
 })
  
